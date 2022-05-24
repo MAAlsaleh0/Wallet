043360b54d2a2cd49ef9c4d23ea70e987337eb17
Mohammed Alsaleh <Malsalehh0@gmail.com>
2022-05-25 01:48:28 +0300

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Mohammed Alsaleh on 24/10/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
